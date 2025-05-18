69faa276fef59ac8a22608117350195e6ef97b56
Gabriela Oria <gaboripin@gmail.com>
2025-05-19 00:24:03 +0200

    Feat(User): Upload profile photo

---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,33 @@
+// file-upload.service.ts
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FileUploadService {
+  private baseURL = environment.baseURL; // Ajusta la URL de tu backend (por ejemplo, Laravel)
+
+  constructor(private http: HttpClient) {}
+
+  uploadFile(file: File): Observable<string> {
+    // Crea un FormData para enviar el archivo al backend
+    const formData = new FormData();
+    formData.append('profile_photo', file);
+ 
+    console.log('Aquí llega: ', formData);
+
+    // Envía la solicitud al endpoint de tu backend
+    return this.http
+      .post(`${this.baseURL}/upload-profile-photo`, formData, { responseType: 'text' })
+      .pipe(
+        map((fileName: string) => {
+          fileName = JSON.parse(fileName);
+          // Construye la URL completa para acceder a la imagen
+          return fileName;
+        })
+      );
+  }
+}
