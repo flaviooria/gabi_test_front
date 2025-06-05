aefd1c5a632eef111452c2cf34c86a1f19f03d2b
Flavio Oria <flavio.oriap@gmail.com>
2025-06-05 19:49:40 +0200

    Init project

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseURL: process.env['BASE_URL_PROD'],
-    stripePublicKey: process.env['STRIPE_PUBLIC_KEY'],
-    cryptoServiceKey: process.env['CRYPTO_KEY']
+    baseURL: "https://haus2houseapi.onrender.com/api",
+    stripePublicKey: "pk_test_51R4hk3I1vsstshh6aPYhn0EMySVkxMP4p0lA2LyHH7t7Oza2Ij8x8khTST91XcHMKw1N0AcQw4NEbImKCY5rFCwq00NfN8YnOL",
+    cryptoServiceKey: "x9k3m7p2q8w4z6t1r5y0u2n4b6v8j0l5"
 }
